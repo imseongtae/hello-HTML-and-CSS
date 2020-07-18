1248fb4c8018554b3a5b02f09d76d3190fcf907a
imseongtae <dev.haemil@gmail.com>
2020-07-18 23:44:21 +0900

    chore: Modify text and code indent

--- contents/haemil_ver_1/js/header.js
+++ contents/haemil_ver_1/js/header.js
@@ -1,134 +1,111 @@
-(function(){
-    
-    var list =  document.querySelector('.lnb');
-    var btn = document.getElementById('toggle_btn');
-    var header = document.getElementById('header');
-    var icon = document.querySelector('.icon');
-    var hamburger = document.querySelector('.hamburger');
-    
-    window.onload = function() {scrollFunction()};
-    window.onscroll = function() {scrollFunction()};
-    
-    // ! 아래 코드는 html을 직접 제어하는 방식에 비해서 오류가 있다. 
-    // function scrollFunction() {
-    //     if(document.documentElement.scrollTop > 70) {
-    //         var header = document.getElementById('header');
-    //         if(!header.classList.contains('navbar_fixed')) {
-    //             header.classList.add('navbar_fixed');
-    //             document.getElementsByTagName('body')[0].setAttribute('class', 'marginTop');
-    //             header.setAttribute('class', 'hidden');
-    //             setTimeout(function() {
-    //                 header.removeAttribute('class');
-    //             }, 40);
-    //         }
-    //     } else {
-    //         var header = document.getElementById('header');
-    //         if(header.classList.contains('navbar_fixed')) {
-    //             header.classList.remove('navbar_fixed');
-    //             document.getElementsByTagName('body')[0].removeAttribute('class');
-    //         }
-    //     }
-    // }
-    
-    function scrollFunction() {
-        if (document.documentElement.scrollTop > 70) {
-          var header = document.getElementById('header');
-          if(!header.classList.contains('navbar_fixed')){
-            header.classList.add('navbar_fixed');
-            document.getElementsByTagName('body')[0].style.marginTop = '70px';
-            header.style.display = 'none';
-            setTimeout(function() {
-              header.style.display = 'block';
-            }, 40);
-          }
-        } else {
-          var header = document.getElementById('header');
-          if(header.classList.contains('navbar_fixed')){
-            header.classList.remove('navbar_fixed');
-            document.getElementsByTagName('body')[0].style.marginTop = '0';
-          }
-        }
-    }
-    
-    
-    
-    
-    // ANCHOR
-    function moveTo(id) {
-        if(id == 'brand') {
-            window.scrollTo(0, 0);
-            hamburger.classList.add('active');
-        } else {
-            window.scrollTo(0, document.getElementById(id).offsetTop - 70);         
-        }
-        document.getElementById('menu').classList.remove('show');
-        hambuger_btn();
-    }
-    // //ANCHOR
-    
-    
-    var menu_toggle = function(event) {
-        list.classList.toggle('show');
-        btn_click_check(event);
-    }
-    
-    
-    var menu_toggle_close = function(event){
-        if(event.target.nodeName == 'LI') {        
-            list.classList.remove('show');
-            hambuger_btn();
-        } 
-    }
-    
-    
-    // * ARIA Button Role
-    var btn_click_check = function(event){
-        event = event || window.event;
-        if(event.target.nodeName == "BUTTON") {
-            var pressed = event.target.getAttribute("aria-pressed") == "true";
-    
-            event.target.setAttribute("aria-pressed", pressed ? "false" : "true" );
-        } else {
-            console.log('클릭한 요소가 버튼이 아님')
-        }
-    }
-    
-    var btn_keyUp_check = function(event){
-        event = event || window.event;
-        if (event.keyCode === 32) { // check for Space key
-            btn_click_check(event);
-        }
-    }
-    // * //ARIA Button Role
-    
-    // * HAMBURGER
-    var hambuger_btn = function() {
-        hamburger.classList.toggle('active');
-    }
-    
-    
-    function init(){
-        // scrollFunction();
-        
-        list.addEventListener('click', menu_toggle_close);
-        btn.addEventListener('click', menu_toggle);
-        btn.addEventListener('click', hambuger_btn);
-        
-        // user-select 를 몰랐을 떄 고생함..;;;
-        // header.addEventListener('onresize', remove_show);    
-        // btn.addEventListener('click', btn_click_check);
-        // btn.addEventListener('keyup', btn_keyUp_check);
-    
-        document.getElementById('navbarBrand').addEventListener('click', moveTo.bind(null, 'brand'));
-        document.getElementById('navbarAbout').addEventListener('click', moveTo.bind(null, 'about'));
-        document.getElementById('navbarSkill').addEventListener('click', moveTo.bind(null, 'skill'));
-        document.getElementById('navbarPortfolio').addEventListener('click', moveTo.bind(null, 'portfolio'));
-        document.getElementById('navbarContact').addEventListener('click', moveTo.bind(null, 'contact'));
-    
-    }
-    
-    init();
+(function () {
 
+	var list = document.querySelector('.lnb');
+	var btn = document.getElementById('toggle_btn');
+	var header = document.getElementById('header');
+	var icon = document.querySelector('.icon');
+	var hamburger = document.querySelector('.hamburger');
+
+	window.onload = function () { scrollFunction() };
+	window.onscroll = function () { scrollFunction() };
+
+
+	function scrollFunction() {
+		if (document.documentElement.scrollTop > 70) {
+			var header = document.getElementById('header');
+			if (!header.classList.contains('navbar_fixed')) {
+				header.classList.add('navbar_fixed');
+				document.getElementsByTagName('body')[0].style.marginTop = '70px';
+				header.style.display = 'none';
+				setTimeout(function () {
+					header.style.display = 'block';
+				}, 40);
+			}
+		} else {
+			var header = document.getElementById('header');
+			if (header.classList.contains('navbar_fixed')) {
+				header.classList.remove('navbar_fixed');
+				document.getElementsByTagName('body')[0].style.marginTop = '0';
+			}
+		}
+	}
+
+
+
+
+	// ANCHOR
+	function moveTo(id) {
+		if (id == 'brand') {
+			window.scrollTo(0, 0);
+			hamburger.classList.add('active');
+		} else {
+			window.scrollTo(0, document.getElementById(id).offsetTop - 70);
+		}
+		document.getElementById('menu').classList.remove('show');
+		hambuger_btn();
+	}
+	// //ANCHOR
+
+
+	var menu_toggle = function (event) {
+		list.classList.toggle('show');
+		btn_click_check(event);
+	}
+
+
+	var menu_toggle_close = function (event) {
+		if (event.target.nodeName == 'LI') {
+			list.classList.remove('show');
+			hambuger_btn();
+		}
+	}
+
+
+	// * ARIA Button Role
+	var btn_click_check = function (event) {
+		event = event || window.event;
+		if (event.target.nodeName == "BUTTON") {
+			var pressed = event.target.getAttribute("aria-pressed") == "true";
+
+			event.target.setAttribute("aria-pressed", pressed ? "false" : "true");
+		} else {
+			console.log('클릭한 요소가 버튼이 아님')
+		}
+	}
+
+	var btn_keyUp_check = function (event) {
+		event = event || window.event;
+		if (event.keyCode === 32) { // check for Space key
+			btn_click_check(event);
+		}
+	}
+	// * //ARIA Button Role
+	// * HAMBURGER
+	var hambuger_btn = function () {
+		hamburger.classList.toggle('active');
+	}
+
+
+	function init() {
+		// scrollFunction();
+
+		list.addEventListener('click', menu_toggle_close);
+		btn.addEventListener('click', menu_toggle);
+		btn.addEventListener('click', hambuger_btn);
+
+		// user-select 를 몰랐을 떄 고생함..;;;
+		// header.addEventListener('onresize', remove_show);    
+		// btn.addEventListener('click', btn_click_check);
+		// btn.addEventListener('keyup', btn_keyUp_check);
+		document.getElementById('navbarBrand').addEventListener('click', moveTo.bind(null, 'brand'));
+		document.getElementById('navbarAbout').addEventListener('click', moveTo.bind(null, 'about'));
+		document.getElementById('navbarSkill').addEventListener('click', moveTo.bind(null, 'skill'));
+		document.getElementById('navbarPortfolio').addEventListener('click', moveTo.bind(null, 'portfolio'));
+		document.getElementById('navbarContact').addEventListener('click', moveTo.bind(null, 'contact'));
+
+	}
+
+	init();
 })()
 
 
--- contents/haemil_ver_1/js/slide.js
+++ contents/haemil_ver_1/js/slide.js
@@ -1,61 +1,61 @@
-(function(){
-    
-    //* SLIDE
-    var imageSlideIndex = 1;
-        
-    var imageSlideTimer = function(){
-        plusImageSlides(1);
+(function () {
+
+  //* SLIDE
+  var imageSlideIndex = 1;
+
+  var imageSlideTimer = function () {
+    plusImageSlides(1);
+  }
+  var imageTimer = setInterval(imageSlideTimer, 3000);
+
+  var plusImageSlides = function (n) {
+    clearInterval(imageTimer);
+    imageTimer = setInterval(imageSlideTimer, 3000);
+
+    showImageSlides(imageSlideIndex += n);
+  }
+  var currentImageSlide = function (n) {
+    clearInterval(imageTimer);
+    imageTimer = setInterval(imageSlideTimer, 3000);
+
+    showImageSlides(imageSlideIndex = n);
+  }
+
+
+  var showImageSlides = function (n) {
+    var slides = document.getElementsByClassName('image_slide');
+    var dots = document.getElementsByClassName('dot');
+
+    if (n > slides.length) { imageSlideIndex = 1 }
+    if (n < 1) { imageSlideIndex = slides.length }
+
+    for (var i = 0; i < slides.length; i++) {
+      // slides[i].setAttribute('class', 'none');
+      // slides[i].style.display = 'none';
+      slides[i].classList.add('none');
+
     }
-    var imageTimer = setInterval(imageSlideTimer, 3000);
-    
-    var plusImageSlides = function(n) {
-        clearInterval(imageTimer);
-        imageTimer = setInterval(imageSlideTimer, 3000);
-    
-        showImageSlides(imageSlideIndex += n);
+    for (var i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(' active', '');
     }
-    var currentImageSlide = function(n) {
-        clearInterval(imageTimer);
-        imageTimer = setInterval(imageSlideTimer, 3000);
-    
-        showImageSlides(imageSlideIndex = n);
-    }
-    
-    
-    var showImageSlides = function(n) {
-        var slides = document.getElementsByClassName('image_slide');
-        var dots = document.getElementsByClassName('dot');
-    
-        if(n > slides.length) {imageSlideIndex = 1}
-        if(n < 1) {imageSlideIndex = slides.length}
-        
-        for(var i = 0; i < slides.length; i++) {
-            // slides[i].setAttribute('class', 'none');
-            // slides[i].style.display = 'none';
-            slides[i].classList.add('none');
-            
-        }
-        for(var i = 0; i < dots.length; i++) {
-            dots[i].className = dots[i].className.replace(' active', '');
-        }
-        // slides[imageSlideIndex - 1].setAttribute('class', 'show');
-        // slides[imageSlideIndex - 1].style.display = 'block';
-        slides[imageSlideIndex - 1].classList.remove('none');
-        dots[imageSlideIndex - 1].className += ' active';
-    }
-    
-    
-    function init() {
-        showImageSlides(imageSlideIndex);
-        
-        document.getElementById('imagePrev').addEventListener('click', plusImageSlides.bind(null, -1));
-        document.getElementById('imageNext').addEventListener('click', plusImageSlides.bind(null, 1));
-        
-        document.getElementById('firstDot').addEventListener('click', currentImageSlide.bind(null, 1));
-        document.getElementById('secondDot').addEventListener('click', currentImageSlide.bind(null,2));
-        document.getElementById('thirdDot').addEventListener('click', currentImageSlide.bind(null,3));    
-    }
-    
-    init();
+    // slides[imageSlideIndex - 1].setAttribute('class', 'show');
+    // slides[imageSlideIndex - 1].style.display = 'block';
+    slides[imageSlideIndex - 1].classList.remove('none');
+    dots[imageSlideIndex - 1].className += ' active';
+  }
+
+
+  function init() {
+    showImageSlides(imageSlideIndex);
+
+    document.getElementById('imagePrev').addEventListener('click', plusImageSlides.bind(null, -1));
+    document.getElementById('imageNext').addEventListener('click', plusImageSlides.bind(null, 1));
+
+    document.getElementById('firstDot').addEventListener('click', currentImageSlide.bind(null, 1));
+    document.getElementById('secondDot').addEventListener('click', currentImageSlide.bind(null, 2));
+    document.getElementById('thirdDot').addEventListener('click', currentImageSlide.bind(null, 3));
+  }
+
+  init();
 
 })()
